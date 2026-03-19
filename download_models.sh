d0a24a887e3990748086c5318281228d5ce32865
zzx <zengzixuan9178@163.com>
2026-03-19 16:02:19 +0800

    models

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,5 @@
-#huggingface-cli download --resume-download sentence-transformers/all-roberta-large-v1 --local-dir ./models/sentence-transformers/all-roberta-large-v1
-#huggingface-cli download --resume-download google-bert/bert-base-uncased --local-dir ./models/google-bert/bert-base-uncased
+huggingface-cli download --resume-download sentence-transformers/all-roberta-large-v1 --local-dir ./models/sentence-transformers/all-roberta-large-v1
+huggingface-cli download --resume-download google-bert/bert-base-uncased --local-dir ./models/google-bert/bert-base-uncased
 #huggingface-cli download --resume-download meta-llama/Llama-2-7b-hf --local-dir ./models/meta-llama/Llama-2-7b-hf
 #huggingface-cli download --resume-download meta-llama/Llama-2-7b-chat --local-dir ./models/meta-llama/Llama-2-7b-chat
 #huggingface-cli download --resume-download osunlp/TableLlama --local-dir ./models/tablellama
